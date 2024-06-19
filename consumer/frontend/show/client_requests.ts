7979f9ea42514d16e665af26c21877900e12a3ac
Yuxuan Xie <teststaybaka@gmail.com>
2024-06-19 16:01:27 -0700

    Re-designed service with start and stop. Release

--- consumer/frontend/show/client_requests.ts
+++ consumer/frontend/show/client_requests.ts
@@ -1,5 +1,19 @@
 import { WebServiceClientInterface, WebServiceClientOptions } from '@selfage/service_descriptor/web_service_client_interface';
-import { SyncMeterReadingRequestBody, SyncMeterReadingResponse, SYNC_METER_READING } from './interface';
+import { StartMeterReadingRequestBody, StartMeterReadingResponse, START_METER_READING, SyncMeterReadingRequestBody, SyncMeterReadingResponse, SYNC_METER_READING, StopMeterReadingRequestBody, StopMeterReadingResponse, STOP_METER_READING } from './interface';
+
+export function startMeterReading(
+  client: WebServiceClientInterface,
+  body: StartMeterReadingRequestBody,
+  options?: WebServiceClientOptions,
+): Promise<StartMeterReadingResponse> {
+  return client.send(
+    {
+      descriptor: START_METER_READING,
+      body,
+    },
+    options,
+  );
+}
 
 export function syncMeterReading(
   client: WebServiceClientInterface,
@@ -9,6 +23,20 @@ export function syncMeterReading(
   return client.send(
     {
       descriptor: SYNC_METER_READING,
+      body,
+    },
+    options,
+  );
+}
+
+export function stopMeterReading(
+  client: WebServiceClientInterface,
+  body: StopMeterReadingRequestBody,
+  options?: WebServiceClientOptions,
+): Promise<StopMeterReadingResponse> {
+  return client.send(
+    {
+      descriptor: STOP_METER_READING,
       body,
     },
     options,
--- consumer/frontend/show/server_handlers.ts
+++ consumer/frontend/show/server_handlers.ts
@@ -1,6 +1,15 @@
 import { ServiceHandlerInterface } from '@selfage/service_descriptor/service_handler_interface';
-import { SYNC_METER_READING, SyncMeterReadingRequestBody, SyncMeterReadingResponse } from './interface';
+import { START_METER_READING, StartMeterReadingRequestBody, StartMeterReadingResponse, SYNC_METER_READING, SyncMeterReadingRequestBody, SyncMeterReadingResponse, STOP_METER_READING, StopMeterReadingRequestBody, StopMeterReadingResponse } from './interface';
 import { ClientSession } from '@phading/user_session_service_interface/client_session';
+
+export abstract class StartMeterReadingHandlerInterface implements ServiceHandlerInterface {
+  public descriptor = START_METER_READING;
+  public abstract handle(
+    loggingPrefix: string,
+    body: StartMeterReadingRequestBody,
+    auth: ClientSession,
+  ): Promise<StartMeterReadingResponse>;
+}
 
 export abstract class SyncMeterReadingHandlerInterface implements ServiceHandlerInterface {
   public descriptor = SYNC_METER_READING;
@@ -9,4 +18,13 @@ export abstract class SyncMeterReadingHandlerInterface implements ServiceHandler
     body: SyncMeterReadingRequestBody,
     auth: ClientSession,
   ): Promise<SyncMeterReadingResponse>;
+}
+
+export abstract class StopMeterReadingHandlerInterface implements ServiceHandlerInterface {
+  public descriptor = STOP_METER_READING;
+  public abstract handle(
+    loggingPrefix: string,
+    body: StopMeterReadingRequestBody,
+    auth: ClientSession,
+  ): Promise<StopMeterReadingResponse>;
 }
